8754b976d1079b17ec59c4ed2df1d568275c3bf6
codeabiswas <axb6972@rit.edu>
2023-04-21 19:14:47 -0400

    Created routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ContactPage from "./pages/ContactPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    // React Router Dom enables client side rendering
+    <BrowserRouter>
+      <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/articles" element={<ArticlesListPage />} />
+        {/* URL parameter :articleId */}
+        <Route path="/articles/:articleId" element={<ArticlePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React-Bootstrap doesn't depend on a very precise version of Bootstrap, so the package does not ship with any CSS. But the stylesheet is required to use the components, and it is added below
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
