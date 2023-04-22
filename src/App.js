922be461e3dd3ceeb9ce7089f559f629bd9cf982
codeabiswas <axb6972@rit.edu>
2023-04-21 20:14:11 -0400

    Successfully created React navbar according to spec and made it compatible with React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
 import ContactPage from "./pages/ContactPage";
+import NavigationBar from "./NavigationBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     // React Router Dom enables client side rendering
     <BrowserRouter>
-      <h1>Hello world!</h1>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
